e5d552a4367170c45c6dacd5a2325734ead0264d
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-08-25 15:17:26 -0700

    Add and configure Storybook (#11)
    * Install Storybook
    * Configure based on https://www.gatsbyjs.org/docs/visual-testing-with-storybook/
    * Set up PostCSS loader in order to support Tailwind
    * Import `gatsby-browser.js` into default storybook config
    * Install viewport addon
    
    ```
    npm install --save-dev @storybook/addon-viewport
    ```

---
+++ .storybook/config.js
@@ -0,0 +1,25 @@
+import { configure } from "@storybook/react"
+import { action } from "@storybook/addon-actions"
+import "../gatsby-browser.js"
+
+// automatically import all files ending in *.stories.js
+const req = require.context("../src", true, /\.stories\.js$/)
+function loadStories() {
+  req.keys().forEach(filename => req(filename))
+}
+
+// gatsby-specific mocks for using the Link API
+// (see https://www.gatsbyjs.org/docs/visual-testing-with-storybook/)
+// mock __PATH_PREFIX__ which Gatsby normally sets for components to use
+global.__PATH_PREFIX__ = ``
+// mock Gatsby loader methods to prevent console errors in storybook
+global.___loader = {
+  enqueue: () => {},
+  hovering: () => {},
+}
+// mock Gatsby ___navigate method to observe the effect of clicking on nav links
+window.___navigate = pathname => {
+  action("gatsbyNavigate:")(pathname)
+}
+
+configure(loadStories, module)
