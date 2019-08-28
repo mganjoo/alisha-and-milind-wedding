39ee814b4955514ffbe34d95e970ecd96c13e6b1
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-08-28 07:46:08 -0700

    Add responsive Tailwind breakpoints to storybook (#25)

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,4 @@
-import { configure } from "@storybook/react"
+import { addParameters, configure } from "@storybook/react"
 import { action } from "@storybook/addon-actions"
 import "../gatsby-browser.js"
 
@@ -7,6 +7,58 @@ const req = require.context("../stories", true, /\.stories\.js$/)
 function loadStories() {
   req.keys().forEach(filename => req(filename))
 }
+
+const responsiveViewports = {
+  mobile_small: {
+    name: "mobile small",
+    styles: {
+      height: "568px",
+      width: "320px",
+    },
+    type: "mobile",
+  },
+  mobile_large: {
+    name: "mobile large",
+    styles: {
+      height: "812px",
+      width: "375px",
+    },
+    type: "mobile",
+  },
+  sm: {
+    name: "sm",
+    styles: {
+      height: "1280px",
+      width: "720px",
+    },
+    type: "mobile",
+  },
+  md: {
+    name: "md",
+    styles: {
+      height: "1024px",
+      width: "768px",
+    },
+    type: "tablet",
+  },
+  lg: {
+    name: "lg",
+    styles: {
+      height: "1366px",
+      width: "1024px",
+    },
+    type: "tablet",
+  },
+  xl: {
+    name: "xl",
+    styles: {
+      height: "800px",
+      width: "1280px",
+    },
+    type: "desktop",
+  },
+}
+addParameters({ viewport: { viewports: responsiveViewports } })
 
 // gatsby-specific mocks for using the Link API
 // (see https://www.gatsbyjs.org/docs/visual-testing-with-storybook/)
