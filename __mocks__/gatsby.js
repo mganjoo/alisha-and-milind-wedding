ecae7ed160f9a7ca5e782ce20ff25779c9c78977
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-08-28 00:20:58 -0700

    Remove Gatsby link mock entirely (#22)
    We might need to set up this mock again some day, but for now its presence presents us from writing tests that render Gatsby Links correctly.

--- __mocks__/gatsby.js
+++ __mocks__/gatsby.js
@@ -4,12 +4,6 @@ const gatsby = jest.requireActual("gatsby")
 module.exports = {
   ...gatsby,
   graphql: jest.fn(),
-  Link: jest.fn().mockImplementation(({ to, className }) =>
-    React.createElement("a", {
-      href: to,
-      className: className,
-    })
-  ),
   StaticQuery: jest.fn(),
   useStaticQuery: jest.fn(),
 }
