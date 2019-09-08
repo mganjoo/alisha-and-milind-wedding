6a40b5244e736d2aa5b021d2c7667398da15882d
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-09-08 00:16:21 -0700

    Add site manifest and offline support (#68)
    * Add basic manifest info
    
    * Revert "Bump gatsby-plugin-offline from 2.2.9 to 3.0.3 (#63)"
    
    This reverts commit 35fefaabf8c831d3788e64827cf3c73bdbc0ecf1.
    
    * Add custom logo graphics

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
     title: `A & M Wedding`,
-    description: `The website for the wedding of Alisha Saxena and Milind Ganjoo.`,
+    description: `Welcome to Alisha Saxena and Milind Ganjoo's wedding website! We're excited to celebrate with you.`,
     author: `@mganjoo`,
   },
   plugins: [
@@ -18,18 +18,38 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Alisha & Milind Wedding`,
+        short_name: `A&M Wedding`,
+        language: `en`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        background_color: "#fffcf7", // off-white
+        theme_color: "#fffcf7", // off-white
+        // Enables "Add to Homescreen" prompt and disables browser UI (including back button)
+        display: "standalone",
+        icon_options: {
+          purpose: `maskable`,
+        },
+        icons: [
+          {
+            src: `/am-logo-96x96.png`,
+            sizes: `96x96`,
+            type: `image/png`,
+          },
+          {
+            src: `/am-logo-192x192.png`,
+            sizes: `192x192`,
+            type: `image/png`,
+          },
+          {
+            src: `/am-logo-512x512.png`,
+            sizes: `512x512`,
+            type: `image/png`,
+          },
+        ],
+        crossOrigin: `use-credentials`,
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    `gatsby-plugin-offline`,
     `gatsby-plugin-postcss`,
     {
       resolve: `gatsby-plugin-purgecss`,
