64b791be80d1ce9d40ac43829a43f8519d8913e1
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-09-19 08:12:54 -0700

    Make Prettier, TypeScript and ESLint work nicer together

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,13 @@ module.exports = {
   globals: {
     __PATH_PREFIX__: true,
   },
-  extends: [`react-app`, `plugin:cypress/recommended`],
+  extends: [
+    "react-app",
+    "plugin:cypress/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+    "prettier/react",
+  ],
   rules: {
     "cypress/assertion-before-screenshot": 1,
   },
