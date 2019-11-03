25e1e8c203b5ad0e2f0bb210a35c8bfbb57f9acb
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-11-02 21:50:20 -0700

    Use images instead of hand drawn CSS for envelope (#262)
    * Use images for animation instead
    
    * Move some classes into main InvitationCard component
    
    * Reorganize styles
    
    * Specify size of letter as percentage
    
    * Specify size of envelope in relative units
    
    * Rearrange demo page
    
    * Make demo page responsive
    
    * More sizes
    
    * More adjustments
    
    * Rotate letter by 90 degree when showing
    
    * Remove extra path from postcss-assets

--- website/src/components/partials/InvitationCard.tsx
+++ website/src/components/partials/InvitationCard.tsx
@@ -20,18 +20,19 @@ interface InvitationCardProps {
 const springConfig = { mass: 5, tension: 300, friction: 80, clamp: true }
 const envelopeRotate = 25 // in degrees
 const envelopeScale = 0.8
-const letterPeakYOffset = 30 // in rem
-const letterFinalYOffset = 2 // in rem
+const letterPeakYOffset = 140 // in %
+const letterFinalYOffset = 0 // in %
 
 // Calculates translateY offset for letter over time. Parabolic curve with Y peaking at t = 0.5
 const interpolateYOffset = (t: number) =>
   (4 * letterPeakYOffset + 2 * letterFinalYOffset) * t * t -
   (4 * letterPeakYOffset + letterFinalYOffset) * t
 const envelopeTransform = (rotateY: any, rotateZ: any, scale: any) =>
-  `perspective(35rem) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg) scale(${scale})`
+  `perspective(55rem) rotateY(${rotateY}deg) rotateZ(${rotateZ}deg) scale(${scale})`
 const letterTransform = (y: any, rotateZ: any, scale: any) =>
-  `translateY(${y}rem) rotateZ(${rotateZ}deg) scale(${scale})`
-const flapTransform = (rotateX: any) => `rotateX(${rotateX}deg)`
+  `translateY(${y}%) rotateZ(${rotateZ}deg) scale(${scale})`
+const flapTransform = (rotateX: any) =>
+  `perspective(55rem) rotateX(${rotateX}deg)`
 
 export default function InvitationCard({
   playing = false,
@@ -50,9 +51,9 @@ export default function InvitationCard({
     }
   }
   const props = useSpring({
-    envelopeRotateY: isAfter("flipped") ? -180 : 0,
+    envelopeRotateY: isAfter("flipped") ? 180 : 0,
     flapZIndex: isAfter("flap-open") ? -1 : 0,
-    flapRotateX: isAfter("flap-open") ? -180 : 0,
+    flapRotateX: isAfter("flap-open") ? 180 : 0,
     letterProgress: isAfter("letter-out") ? 1 : 0,
     config: springConfig,
     onRest: transition,
@@ -61,37 +62,14 @@ export default function InvitationCard({
   })
 
   return (
-    <animated.div
-      className="envelope"
-      style={{
-        transform: interpolate(
-          [
-            props.envelopeRotateY,
-            props.letterProgress.interpolate({
-              // rotateZ
-              range: [0, 0.5, 1],
-              output: [0, 0, -envelopeRotate],
-            }),
-            props.letterProgress.interpolate({
-              // scale
-              range: [0, 0.5, 1],
-              output: [1, 1, envelopeScale],
-            }),
-          ],
-          envelopeTransform
-        ),
-      }}
-    >
-      <div className="front">
-        <p className="address">{invitation.partyName}</p>
-      </div>
-      <div className="back">
+    <div className="envelope-wrapper-dimensions">
+      <div className="envelope-dimensions">
         <animated.div
-          className="letter"
+          className="c-flippable"
           style={{
             transform: interpolate(
               [
-                props.letterProgress.interpolate(interpolateYOffset),
+                props.envelopeRotateY,
                 props.letterProgress.interpolate({
                   // rotateZ
                   range: [0, 0.5, 1],
@@ -100,27 +78,81 @@ export default function InvitationCard({
                 props.letterProgress.interpolate({
                   // scale
                   range: [0, 0.5, 1],
-                  output: [1, 1, 1 / envelopeScale],
+                  output: [1, 1, envelopeScale],
                 }),
               ],
-              letterTransform
+              envelopeTransform
             ),
-            zIndex: props.letterProgress.interpolate(p => (p > 0.5 ? 1 : 0)),
           }}
         >
-          <p>Welcome to our wedding</p>
+          <div
+            className="front flex items-center justify-center"
+            style={{ backgroundImage: "url('/invitation/front-base.png')" }}
+          >
+            <p className="font-serif text-lg text-gray-900 tracking-tight">
+              {invitation.partyName}
+            </p>
+          </div>
+          <div
+            className="back"
+            style={{ backgroundImage: "url('/invitation/back-base.png')" }}
+          >
+            <animated.div
+              className="letter-dimensions bg-off-white rounded-sm shadow-md border border-orange-200 px-3 py-8"
+              style={{
+                transform: interpolate(
+                  [
+                    props.letterProgress.interpolate(interpolateYOffset),
+                    props.letterProgress.interpolate({
+                      // rotateZ
+                      range: [0, 0.5, 1],
+                      output: [0, 0, -envelopeRotate + 90],
+                    }),
+                    props.letterProgress.interpolate({
+                      // scale
+                      range: [0, 0.5, 1],
+                      output: [1, 1, 1 / envelopeScale],
+                    }),
+                  ],
+                  letterTransform
+                ),
+                zIndex: props.letterProgress.interpolate(p =>
+                  p > 0.5 ? 1 : 0
+                ),
+              }}
+            >
+              <p>Welcome to our wedding</p>
+            </animated.div>
+            <div
+              className="c-full-area"
+              style={{
+                backgroundImage: "url('/invitation/back-bottom-flaps.png')",
+              }}
+            ></div>
+            <animated.div
+              className="c-flippable"
+              style={{
+                transform: props.flapRotateX.interpolate(flapTransform),
+                transformOrigin: "center top",
+                zIndex: props.flapZIndex,
+              }}
+            >
+              <div
+                className="front"
+                style={{
+                  backgroundImage: "url('/invitation/front-flap.png')",
+                }}
+              ></div>
+              <div
+                className="back"
+                style={{
+                  backgroundImage: "url('/invitation/back-flap.png')",
+                }}
+              ></div>
+            </animated.div>
+          </div>
         </animated.div>
-        <div className="flap left-flap"></div>
-        <div className="flap right-flap"></div>
-        <div className="flap bottom-flap"></div>
-        <animated.div
-          className="flap top-flap"
-          style={{
-            transform: props.flapRotateX.interpolate(flapTransform),
-            zIndex: props.flapZIndex,
-          }}
-        ></animated.div>
       </div>
-    </animated.div>
+    </div>
   )
 }
--- website/src/pages-full-site/demo.tsx
+++ website/src/pages-full-site/demo.tsx
@@ -1,23 +1,29 @@
 import React, { useState } from "react"
 import InvitationCard from "../components/partials/InvitationCard"
 import Button from "../components/ui/Button"
+import Helmet from "react-helmet"
 
 export default function DemoPage() {
   const [playing, setPlaying] = useState(false)
   const [direction, setDirection] = useState(1)
   return (
-    <main className="bg-orange-200 h-screen flex items-center">
-      <div className="flex flex-col px-2">
-        <Button onClick={() => setPlaying(!playing)}>
-          {playing ? "Pause" : "Play"}
-        </Button>
-        <Button onClick={() => setDirection(-direction)}>
-          {direction > 0 ? "Switch to reverse" : "Switch to forward"}
-        </Button>
-      </div>
-      <div className="ml-12">
-        <InvitationCard playing={playing} reverse={direction < 0} />
-      </div>
-    </main>
+    <>
+      <Helmet>
+        <body className="bg-orange-200"></body>
+      </Helmet>
+      <main className="flex flex-col">
+        <div className="flex justify-start py-4">
+          <Button onClick={() => setPlaying(!playing)}>
+            {playing ? "Pause" : "Play"}
+          </Button>
+          <Button className="ml-4" onClick={() => setDirection(-direction)}>
+            Switch: {direction > 0 ? "reverse" : "forward"}
+          </Button>
+        </div>
+        <div className="flex-1 flex justify-center items-center">
+          <InvitationCard playing={playing} reverse={direction < 0} />
+        </div>
+      </main>
+    </>
   )
 }
