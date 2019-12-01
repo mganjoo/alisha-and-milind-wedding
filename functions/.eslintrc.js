ce1a100f3e6012eab3856ddeb7b7ca11f55534e5
Milind Ganjoo <milind.ganjoo@gmail.com>
2019-11-30 22:03:59 -0800

    Add CI tests for Firebase (#394)
    * Add CI tests for Firebase
    
    * Bump Node version
    
    * Actually add eslintrc
    
    * Set port before running emulator

---
+++ functions/.eslintrc.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "prettier",
+    "prettier/@typescript-eslint",
+  ],
+  parserOptions: {
+    project: "./tsconfig.eslint.json",
+  },
+  plugins: ["no-only-tests"],
+  rules: {
+    "no-only-tests/no-only-tests": "error",
+    "@typescript-eslint/no-floating-promises": "error",
+    "@typescript-eslint/no-explicit-any": "off",
+  },
+}
