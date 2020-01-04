58526b66a1e1b5b5b2c7cf8110a6c1d85af2401c
Milind Ganjoo <milind.ganjoo@gmail.com>
2020-01-04 22:54:54 +0530

    Support publishing deploys after Netlify finishes building

--- website/trigger-github.sh
+++ website/trigger-github.sh
@@ -5,14 +5,14 @@
 
 if [ "$PULL_REQUEST" = true ]; then
   echo "Ignoring GitHub trigger for pull request build"
-elif [ "$DEPLOY_URL" != "" ] && [ "$GITHUB_TRIGGER_TOKEN" != "" ]; then
-  echo "Triggering GitHub build against deployed url $DEPLOY_URL"
+elif [ "$DEPLOY_ID" != "" ] && [ "$GITHUB_TRIGGER_TOKEN" != "" ]; then
+  echo "Triggering GitHub build against deployed ID $DEPLOY_ID"
 
   curl_command="curl -X POST \
     -H \"Accept: application/vnd.github.everest-preview+json\" \
     -H \"Authorization: token <GITHUB_TRIGGER_TOKEN>\" \
     -H \"Content-Type: application/json\" \
-    --data '{\"event_type\": \"externalUrl_${DEPLOY_URL}\"}' \
+    --data '{\"event_type\": \"externalUrl_${DEPLOY_ID}\"}' \
     https://api.github.com/repos/mganjoo/alisha-and-milind-wedding/dispatches"
 
   echo "Running cURL command:"
@@ -20,6 +20,6 @@ elif [ "$DEPLOY_URL" != "" ] && [ "$GITHUB_TRIGGER_TOKEN" != "" ]; then
 
   eval ${curl_command/<GITHUB_TRIGGER_TOKEN>/$GITHUB_TRIGGER_TOKEN}
 else
-  echo "Either DEPLOY_URL or GITHUB_TRIGGER_TOKEN environment variable was not provided to GitHub trigger job"
+  echo "Either DEPLOY_ID or GITHUB_TRIGGER_TOKEN environment variable was not provided to GitHub trigger job"
   exit 1
 fi
